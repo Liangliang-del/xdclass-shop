cc56bb42cc08023b83064fd8569b1793987c3206
hlchen02 <hlchen02@wisedu.com>
2021-02-02 16:42:05 +0800

    初始化提交

---
+++ src/main/java/com/chl/xdclassshop/XdclassShopApplication.java
@@ -0,0 +1,13 @@
+package com.chl.xdclassshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdclassShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XdclassShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chl/xdclassshop/XdclassShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chl.xdclassshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XdclassShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
